136bbb41c657a5cba1252b3b85ada99bb92473e5
Annie Huang <annie.hn@gmail.com>
2024-04-27 17:16:33 +1000

    !!!!Configure Jest in Vite project

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import './App.css';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import BasicTabs from './components/Tabs';
+import reactIcon from './assets/react.svg';
 
 function App() {
   return (
-    <div>
+    <div className='app'>
+      <img src={reactIcon} alt='' />
       <Stack spacing={2} direction='row'>
         <Button variant='text'>Text</Button>
         <Button variant='contained'>Contained</Button>
